b6fb5b61eb4784783436906a9b15f295e07432ae
Rahimboy-252 <140332035+Rahimboy-252@users.noreply.github.com>
2025-11-11 15:09:53 +0500

    Add files via upload

--- game.js
+++ game.js
@@ -4,15 +4,20 @@ let selectedColor
 let gamePattern = []
 let userClickedPattern = []
 let level = 0
+let gameIsRunning = false
+let highScore = 0
 
 function nextSequence() {
-    let randomNumber = Math.floor(Math.random() * 4)
-    randomChoosenColor = buttonColors[randomNumber]
-    gamePattern.push(randomChoosenColor)
-    $("#" + randomChoosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100)
-    playSound(randomChoosenColor)
-    level++
-    $('h1').text("Level: " + level)
+    if (gameIsRunning) {
+        let randomNumber = Math.floor(Math.random() * 4)
+        randomChoosenColor = buttonColors[randomNumber]
+        gamePattern.push(randomChoosenColor)
+        $("#" + randomChoosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+        playSound(randomChoosenColor)
+        level++
+        $('h1').text("Level: " + level)
+        userClickedPattern = []
+    }
 }
 
 function playSound(color) {
@@ -27,30 +32,54 @@ function animatePress(color) {
         $("#" + color).removeClass("pressed")
     }, 100)
 }
+
 $('body').keypress(function () {
     if (level == 0) {
         nextSequence()
+        gameIsRunning = true
     }
 })
 
 $(".btn").click(function (e) {
-    selectedColor = e.target.id
-    userClickedPattern.push(selectedColor)
-    playSound(selectedColor)
-    animatePress(selectedColor)
-    checkAnswer(userClickedPattern.length - 1)
+    if (gameIsRunning) {
+        selectedColor = e.target.id
+        userClickedPattern.push(selectedColor)
+        playSound(selectedColor)
+        animatePress(selectedColor)
+        checkAnswer(userClickedPattern.length - 1)
+    }
+
 })
-function checkAnswer(level){
-    if (userClickedPattern[level] == gamePattern[level]){
-        console.log(userClickedPattern)
-        console.log(gamePattern)
-        if(userClickedPattern.length == gamePattern.length){
-            console.log("succes")
-            setTimeout(function(){
+function checkAnswer(level) {
+    if (userClickedPattern[level] == gamePattern[level]) {
+        if (userClickedPattern.length == gamePattern.length) {
+            setTimeout(function () {
                 nextSequence()
             }, 1000)
-        }else{
-            console.log(fail)
         }
+    } else {
+        gameOver()
+    }
+
+}
+
+function gameOver() {
+    playSound("wrong")
+    $("body").addClass("game-over")
+    setTimeout(function () {
+        $("body").removeClass("game-over")
+    }, 500)
+    $("h1").text("Game over, please press any key to restart")
+    checkHighest(level)
+    $("#high-score").text("Highest score: " + highScore)
+    level = 0
+    userClickedPattern = []
+    gamePattern = []
+    gameIsRunning = false
+}
+
+function checkHighest(score) {
+    if (highScore < score) {
+        highScore = score
     }
 }
